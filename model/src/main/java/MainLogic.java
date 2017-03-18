3089514f1f03fb1b46feb5a4dc0a6c10cdb099ea
faystmax <faystmax@gmail.com>
2017-03-18 12:55:24 +0600

    Создал класс MainLogic и тесты на него
    Signed-off-by: faystmax <faystmax@gmail.com>

---
+++ model/src/main/java/MainLogic.java
@@ -0,0 +1,8 @@
+/**
+ * Created with IntelliJ IDEA
+ * User: Maxim Amosov <faystmax@gmail.com>
+ * Date: 18.03.2017
+ * Time: 12:49
+ */
+public class MainLogic {
+}
---
+++ model/src/test/java/MainLogicTest.java
@@ -0,0 +1,17 @@
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created with IntelliJ IDEA
+ * User: Maxim Amosov <faystmax@gmail.com>
+ * Date: 18.03.2017
+ * Time: 12:53
+ */
+public class MainLogicTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+}

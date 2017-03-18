5d97066302d8d5a741fce4408295bfae333d9b00
faystmax <faystmax@gmail.com>
2017-03-18 13:58:39 +0600

    Написал тесты на резмерность игры(demension)
    Signed-off-by: faystmax <faystmax@gmail.com>

--- model/src/main/java/MainLogic.java
+++ model/src/main/java/MainLogic.java
@@ -3,6 +3,18 @@
  * User: Maxim Amosov <faystmax@gmail.com>
  * Date: 18.03.2017
  * Time: 12:49
+ *
+ *  Класс представляет собой основную логику игры (Tic-tac-toe) Крестики-нолики
  */
 public class MainLogic {
+
+    private int demension;          ///< Размерность игры (3x3, 6x6) , должна быть  3 <= demension <= 10
+
+    public int getDemension() {
+        return demension;
+    }
+
+    public void setDemension(int demension) {
+        this.demension = demension;
+    }
 }
--- model/src/test/java/MainLogicTest.java
+++ model/src/test/java/MainLogicTest.java
@@ -1,4 +1,5 @@
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -9,9 +10,41 @@ import static org.junit.Assert.*;
  * Time: 12:53
  */
 public class MainLogicTest {
+
+    MainLogic mainLogic;
+
     @Before
     public void setUp() throws Exception {
+        mainLogic = new MainLogic();
+    }
 
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setDimension_0() {
+        mainLogic.setDemension(0);
+    }
+
+    @Test
+    public void setDimension_3() {
+        mainLogic.setDemension(3);
+        assertEquals(3,mainLogic.getDemension());
+    }
+
+    @Test
+    public void setDimension_10() {
+        mainLogic.setDemension(10);
+        assertEquals(10,mainLogic.getDemension());
+    }
+
+    @Test
+    public void setDimension_6() {
+        mainLogic.setDemension(6);
+        assertEquals(6,mainLogic.getDemension());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setDimension_100() {
+        mainLogic.setDemension(100);
     }
 
 }
